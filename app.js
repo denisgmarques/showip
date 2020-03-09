968f2d19ced82efe4014b527906e349cd7a98d22
Denis Giovan Marques <denis.marques@icolabora.com.br>
2020-03-09 18:28:32 -0300

    First commit with Dockerfile and Kubernetes yaml.

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import db from './db/db';
+var getIP = require('ipware')().get_ip;
+
+// Set up the express app
+const app = express();
+
+// Return ip address
+app.get('/', (req, res) => {
+  var ipInfo = getIP(req);
+  res.status(200).json(ipInfo);
+});
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+});
---
+++ docker/build-docker-image.sh
@@ -0,0 +1,10 @@
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+echo $PACKAGE_VERSION
+
+docker build . 
